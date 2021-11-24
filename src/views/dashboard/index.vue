8d4c2d5e5a9cf40b49298a809af2002f9d441648
Mr.chen <73580405+chen-Get@users.noreply.github.com>
2021-11-24 15:43:17 +0800

    Add files via upload

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      currentRole: 'adminDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  }
+}
+</script>
